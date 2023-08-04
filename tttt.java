2f21cf94f4932b1418ab5c1b648209fdac8cc1b4
shuaigeyong <chinyong7027@gmail.com>
2023-08-04 21:40:46 +0800

    p

--- tttt.java
+++ tttt.java
@@ -1,7 +1,7 @@
 public class tttt{
 	public static void main(String[] args){
 
-		System.out.println("1dadasddasdasdasdasdttt2200000000111111111111011100");
+		System.out.println("31dadasddasdasdasdasdttt2200000000111111111111011100");
 
 	}
 }		
