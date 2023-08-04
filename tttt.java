bf6d5143c50bf435983aed5930edc3ef55eacbae
shuaigeyong <chinyong7027@gmail.com>
2023-08-04 21:48:46 +0800

    3

--- tttt.java
+++ tttt.java
@@ -2,11 +2,8 @@ public class tttt{
 	public static void main(String[] args){
 
 
-<<<<<<< HEAD
-		System.out.println("master31dadasddasdasdasdasdttt2200000000111111111111011100");
-=======
-		System.out.println("clone1dadasddasdasdasdasdttt2200000000111111111111011100");
->>>>>>> 22042bc45822cef82974bc50d1546bd93796f402
+
+		System.out.println("mastermaster31dadasddasdasdasdasdttt2200000000111111111111011100");
 
 
 	}
